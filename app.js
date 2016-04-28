bbd9a916b03b53e3b240161b0814b6e4a56d50f1
Britta Rodenbeck <rodenbeck.britta@gmail.com>
2016-04-28 14:15:13 -0400

    created reservation object

--- app.js
+++ app.js
@@ -1,9 +1,31 @@
 $(function() {
 
+	var seatNumber;
+
 	$("form").hide();
 	$(".available").on("click", function() {
 		$("form").show();
+		seatNumber = $(this).attr("id");
+		$(".yourSeat").text("seat #" + seatNumber + ":");
+
 	});
 
+	var reservation = {
+
+	};
+
+	$("#button").on("click", function() {
+		var name = $("#name").val();
+		var phone = $("#phone_number").val();
+		var email = $("#email").val();
+		var yourSeat = $(".yourSeat").text();
+
+		reservation.seat = yourSeat;
+		reservation.name = name;
+		reservation.phone = phone;
+		reservation.email = email;
+
+		$("#formArea").html("<p>Thank you for your reservation!</p>")
+	});
 
 });
